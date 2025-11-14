5cbd7855af5ee392a9786046dc0314d7f007ac19
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-11-14 21:38:37 +0200

    finished boxesClass method

--- image-info.py
+++ image-info.py
@@ -5,3 +5,8 @@ class ImageInfo :
         self.results = self.model(pathImage)[0]
         self.boxes = self.results.boxes
         self.allNames = self.results.names
+    
+    def boxesClass(self, class_name):
+        cls = self.boxes.cls.cpu().numpy()
+        indices = [i for i, c in enumerate(cls) if self.allNames[c] == class_name]
+        return indices
