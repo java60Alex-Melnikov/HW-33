4005853e7dddee3c53bcee7ae11102401e403460
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-11-15 08:38:13 +0200

    finished dataFrame method

--- image-info.py
+++ image-info.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import pandas as pd
 class ImageInfo :
     def __init__(self, pathImage):
         self.model = YOLO('yolov8m-seg.pt')
@@ -17,3 +18,13 @@ class ImageInfo :
         cls = self.boxes.cls.cpu().numpy()[index]
         class_name = self.allNames[cls]
         return (*xyxy, conf, class_name)
+    
+    def dataFrame(self):
+        xyxy = self.boxes.xyxy.cpu().numpy()
+        cls = self.boxes.cls.cpu().numpy()
+        conf = self.boxes.conf.cpu().numpy()
+        names = [self.allNames[c] for c in cls]
+        df = pd.DataFrame(xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
+        df["name"] = names
+        df["confidence"] = conf
+        return df
